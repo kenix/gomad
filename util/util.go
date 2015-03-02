0a5e4ce53a1ca04c9e4bde657207ff4f154cf2c6
mentopolis <zhongda.zhao@gmail.com>
2015-03-02 22:03:26 +0100

    add common loggers

--- util/util.go
+++ util/util.go
@@ -1,5 +1,38 @@
 package util
 
+import (
+	"io"
+	"log"
+)
+
+func GoNonBlocking(f func() interface{}) (ch chan interface{}) {
+	go func() {
+		select {
+		case ch <- f():
+		default:
+		}
+	}()
+	return
+}
+
+var Lt *log.Logger
+var Li *log.Logger
+var Lw *log.Logger
+var Le *log.Logger
+var Lf *log.Logger
+var Lc *log.Logger
+
+func InitLoggers(w io.Writer) {
+	Lt = log.New(w, "T ", log.LstdFlags|log.Lshortfile)
+	Li = log.New(w, "I ", log.LstdFlags|log.Lshortfile)
+	Lw = log.New(w, "W ", log.LstdFlags|log.Lshortfile)
+	Le = log.New(w, "E ", log.LstdFlags|log.Lshortfile)
+	Lf = log.New(w, "F ", log.LstdFlags|log.Lshortfile)
+	Lc = log.New(w, "C ", log.LstdFlags|log.Lshortfile)
+}
+
+type Cue struct{}
+
 func LeadingZeros(d interface{}) int {
 	switch t := d.(type) {
 	case uint8:
