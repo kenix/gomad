aa0b94fb1dd4a0846bb371d53792f63b4409a78f
kenix <zhongda.zhao@gmail.com>
2015-03-14 03:36:57 +0100

    add closest pair impl.

--- pair/pair.go
+++ pair/pair.go
@@ -1,7 +1,10 @@
 package pair
 
-import "math"
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"sort"
+)
 
 type Point struct {
 	x, y float64
@@ -38,7 +41,7 @@ func NewPoints(ps []*Point) Points {
 }
 
 func (ps Points) Len() int {
-	return len([]*Point(ps))
+	return len(ps)
 }
 
 func (ps Points) Swap(i, j int) {
@@ -61,7 +64,103 @@ func (ps ByY) Less(i, j int) bool {
 	return ps.Points[i].y < ps.Points[j].y
 }
 
-func ClosestPair(ps Points) (*Point, *Point) {
-	// TODO divide and conquer
-	return nil, nil
+type Indices struct {
+	Indices []int
+	sort.Interface
+}
+
+func NewIndices(underlying sort.Interface) Indices {
+	indices := []int(nil)
+	for i := 0; i < underlying.Len(); i++ {
+		indices = append(indices, i)
+	}
+	return Indices{indices, underlying}
+}
+
+func (ids Indices) Swap(i, j int) {
+	ids.Indices[i], ids.Indices[j] = ids.Indices[j], ids.Indices[i]
+}
+
+func (ids Indices) Less(i, j int) bool {
+	return ids.Interface.Less(ids.Indices[i], ids.Indices[j])
+}
+
+func ClosestPair(ps []*Point) (*Point, *Point, float64) {
+	switch len(ps) {
+	case 0, 1:
+		return nil, nil, math.MaxFloat64
+	case 2:
+		return ps[0], ps[1], Distance(ps[0], ps[1])
+	}
+
+	pa := NewPoints(ps)
+	sort.Sort(ByX{pa})
+	pyi := NewIndices(ByY{pa})
+	sort.Sort(pyi)
+
+	return dccPair(ps, pyi.Indices, pa)
+}
+
+func dccPair(ps []*Point, pyi []int, pa Points) (*Point, *Point, float64) {
+	switch len(ps) {
+	case 2:
+		return ps[0], ps[1], Distance(ps[0], ps[1])
+	case 3:
+		d01 := Distance(ps[0], ps[1])
+		d02 := Distance(ps[0], ps[2])
+		d12 := Distance(ps[1], ps[2])
+		if d01 <= d02 {
+			if d12 <= d01 {
+				return ps[1], ps[2], d12
+			}
+			return ps[0], ps[1], d01
+		}
+		if d12 <= d02 {
+			return ps[1], ps[2], d12
+		}
+		return ps[0], ps[2], d02
+	}
+
+	// TODO use goroutine
+	pl1, pl2, dl := dccPair(ps[:len(ps)/2], pyi, pa)
+	pr1, pr2, dr := dccPair(ps[len(ps)/2:], pyi, pa)
+	d := math.Min(dl, dr)
+
+	sp := []*Point(nil)
+	px := ps[len(ps)/2]
+	for i := 0; i < len(pyi); i++ {
+		p := pa[pyi[i]]
+		if math.Abs(p.x-px.x) <= d && math.Abs(p.y-px.y) <= d {
+			sp = append(sp, p)
+		}
+	}
+	ps1, ps2, ds := closestSplitPair(sp)
+	if ds <= d {
+		return ps1, ps2, ds
+	}
+	if dl <= dr {
+		return pl1, pl2, dl
+	}
+	return pr1, pr2, dr
+}
+
+func closestSplitPair(ps []*Point) (ps1 *Point, ps2 *Point, d float64) {
+	switch len(ps) {
+	case 0, 1:
+		return nil, nil, math.MaxFloat64
+	case 2:
+		return ps[0], ps[1], Distance(ps[0], ps[1])
+	}
+
+	d = math.MaxFloat64
+	n := int(math.Min(7, float64(len(ps))))
+	for i := 0; i <= len(ps)-n; i++ {
+		for j := i + 1; j < n; j++ {
+			ds := Distance(ps[i], ps[j])
+			if ds < d {
+				ps1, ps2, d = ps[i], ps[j], ds
+			}
+		}
+	}
+	return
 }
--- pair/pair_test.go
+++ pair/pair_test.go
@@ -6,6 +6,57 @@ import (
 	"testing"
 )
 
+func TestClosestPair(t *testing.T) {
+	cases := []struct {
+		in  []*Point
+		out float64
+	}{
+		{
+			[]*Point{
+				NewPoint(2, 3), NewPoint(12, 30), NewPoint(40, 50),
+				NewPoint(5, 1), NewPoint(12, 10), NewPoint(3, 4),
+				NewPoint(50, 10), NewPoint(120, 100), NewPoint(30, 40),
+				NewPoint(52, 12), NewPoint(122, 102), NewPoint(32, 42),
+				NewPoint(54, 14), NewPoint(124, 104), NewPoint(34, 44),
+				NewPoint(56, 16), NewPoint(126, 106), NewPoint(36, 46),
+				NewPoint(58, 18), NewPoint(128, 108), NewPoint(38, 48),
+				NewPoint(60, 20), NewPoint(130, 110),
+			},
+			1.4142135623730951,
+		},
+	}
+
+	for _, c := range cases {
+		p1, p2, d := ClosestPair(c.in)
+		if d != c.out {
+			t.Errorf("wanted %v got %v %s %s\n", c.out, d, p1, p2)
+		}
+	}
+}
+
+func TestIndices(t *testing.T) {
+	cases := []struct {
+		in  Indices
+		out []int
+	}{
+		{
+			NewIndices(ByX{NewPoints([]*Point{NewPoint(2, 1), NewPoint(0, 3), NewPoint(1, 2)})}),
+			[]int{1, 2, 0},
+		},
+		{
+			NewIndices(ByY{NewPoints([]*Point{NewPoint(2, 1), NewPoint(0, 3), NewPoint(1, 2)})}),
+			[]int{0, 2, 1},
+		},
+	}
+
+	for _, c := range cases {
+		sort.Sort(c.in)
+		if !reflect.DeepEqual(c.in.Indices, c.out) {
+			t.Errorf("wanted %v got %v\n", c.out, c.in.Indices)
+		}
+	}
+}
+
 func TestPointsSort(t *testing.T) {
 	cases := []struct {
 		in   Points
