83f82ef6fdb1a5acccc8bab80f5e95d1899e1500
kenix <zhongda.zhao@gmail.com>
2015-05-15 12:10:23 +0200

    export method instead of var, add comments

--- prime/prime.go
+++ prime/prime.go
@@ -1,10 +1,37 @@
+// package prime implements several prime number sieves
 package prime
 
+// Sieve gets an upper bound as input and returns a channel, from which all prime
+// numbers upto that upper bound can be drained, once.
 type Sieve interface {
 	All(uint64) <-chan uint64
 }
 
-var SieveEratosthenes Sieve = &eratosthenes{}
-var SieveEratosthenesO Sieve = &eratosthenes_o{}
-var SieveAtkin Sieve = &atkin{}
-var SieveSundaram Sieve = &sundaram{}
+var sieveEratosthenes Sieve = &eratosthenes{}
+
+// SieveEratosthenes implements the sieve of Eratosthenes using exclusion-channel
+// algorithm adapted from Rob Pike's presentation of Concurrency Pattern
+func SieveEratosthenes() Sieve {
+	return sieveEratosthenes
+}
+
+var sieveEratosthenesO Sieve = &eratosthenes_o{}
+
+// SieveEratosthenesO implements the optimized Eratosthenes sieve
+func SieveEratosthenesO() Sieve {
+	return sieveEratosthenesO
+}
+
+var sieveAtkin Sieve = &atkin{}
+
+// TODO not implemented yet
+func SieveAtkin() Sieve {
+	return sieveAtkin
+}
+
+var sieveSundaram Sieve = &sundaram{}
+
+// SieveSundaram implements the prime sieve of Sundaram
+func SieveSundaram() Sieve {
+	return sieveSundaram
+}
